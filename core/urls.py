fdc5f4989520e6e107a2a57fc3acbb612b94c011
jhmiante <jhmiante@gmail.com>
2024-07-03 23:08:14 -0300

    Template NPS

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import index_view
+from .views import index_view, nps_view
 
 urlpatterns = [
     path("", index_view, name='index_view'),
 
+    path("nps/", nps_view, name='nps_view'),
+    path("nps/<int:filial>/", nps_view, name='nps_view'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+def nps_view(request, filial=1):
+    return render(request, 'nps.html')
